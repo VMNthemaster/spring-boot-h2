013cc2f9da6cc4c70c28e64a7593cd3e0db27b7b
Varad NADKARNI <varad.nadkarni@amadeus.com>
2024-08-22 12:33:44 +0530

    crud repository functions working correctly

--- src/main/java/dev/vmn/h2_demo/controller/StudentController.java
+++ src/main/java/dev/vmn/h2_demo/controller/StudentController.java
@@ -1,18 +1,25 @@
 package dev.vmn.h2_demo.controller;
 
 import dev.vmn.h2_demo.model.Student;
-import dev.vmn.h2_demo.model.Teacher;
 import dev.vmn.h2_demo.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class StudentController {
     @Autowired
     private StudentRepository studentRepository;
+
+    @GetMapping("/getStudents")
+    public List<Student> getStudents(){
+        return studentRepository.findAll();
+    }
 
     @PostMapping("/addStudent")
     public ResponseEntity<String> addStudent (@RequestBody Student student){
--- src/main/java/dev/vmn/h2_demo/repository/StudentRepository.java
+++ src/main/java/dev/vmn/h2_demo/repository/StudentRepository.java
@@ -3,5 +3,9 @@ package dev.vmn.h2_demo.repository;
 import dev.vmn.h2_demo.model.Student;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface StudentRepository extends CrudRepository<Student, Integer> {
+    List<Student> findAll();
+    Student findByStudent_Id(int studentId);
 }
